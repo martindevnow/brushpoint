153fba6d5b82c10e8b55c04cc3d7ba42dfdff220
Ben Martin (Dell) <benjaminm@oriondas.com>
2015-03-17 18:27:52 -0400

    Processing

--- app/Handlers/Commands/ProcessPaymentStatusCommandHandler.php
+++ app/Handlers/Commands/ProcessPaymentStatusCommandHandler.php
@@ -33,16 +33,56 @@ class ProcessPaymentStatusCommandHandler {
 	 */
 	public function handle(ProcessPaymentStatusCommand $command)
 	{
+        // PAYMENT
+        // try to find the payment to see if it is a new payment or what not.
+
+        // IF NEW ---
+        // PAYER
+        // search for payer (by payer id)
+            // create if doesn't exist
+
+        // ADDRESS
+        // add address if not completely the same and associate it to the payer
+        // an address can only have one payer,
+        // but an address can be associated to many payments.
+        // need to figure this part out
+
+        // TRANSACTIONS
+        // add the transactions and sold items to the DB
+        // associate together
+
+        // PAYMENT
+        // add the payment to the DB using the payment_id
+        // associate the payment to the Payer
+        //                              Transactions
+
+        // END IF NEW ---
+
+
+        /*
+         * Payer
+         *      Address
+         *      Address
+         *
+         *
+         */
+
+
+
         $PPpayment = Payment::get($command->paymentId, $this->api);
         Log::info(print_r($PPpayment,1));
 
         $paymentRepo = new \Martin\Ecom\Repositories\PaymentRepository();
-
         $addressRepo = new \Martin\Core\Repositories\AddressRepository();
-        $address = $addressRepo->findOrCreateFromPayPal($PPpayment->getPayer());
+        $payerRepo = new \Martin\Ecom\Repositories\PayerRepository();
 
 
-        $address->save();
+
+        $payer = $payerRepo->findOrCreateFromPayPal($PPpayment->getPayer());
+        $address = $addressRepo->findOrCreateFromPayPal($payer, $PPpayment->get);
+
+
+
 
 
         // return $payment;
