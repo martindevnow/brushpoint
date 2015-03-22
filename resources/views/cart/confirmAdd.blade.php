369e6a483d1582c8892ccdb14c7f4a64d6cc84eb
Ben Martin (Dell) <benjaminm@oriondas.com>
2015-03-22 10:31:18 -0400

    Confirm to add to cart

--- resources/views/cart/confirmAdd.blade.php
+++ resources/views/cart/confirmAdd.blade.php
@@ -9,8 +9,8 @@
     $page['short_title'] = "Cart";
     $page['short_description'] = "Confirm Quantity";
 
-     $showCart = true;
-     $hideBreadcrumb = true;
+    $showCart = true;
+    $hideBreadcrumb = true;
 
     ?>
     @include('zeina.top-title-wrapper', ['page' => $page])
@@ -20,21 +20,25 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div>
+        <div class="col-md-2"></div>
+        <div class="col-md-8">
             <h2 class="left-text">{{ $product->name }}</h2>
         </div>
+        <div class="col-md-2"></div>
     </div>
-   <div class="row">
-        <div class="col-sm-6 col-md-4">
-           <a href="/purchase/id-{{ $product->id }}"><img src="/images/brushpoint/purchase/{{ $product->sku }}-240.png" alt="{{ $product->name }}"></a>
-        </div>
-        {!! Form::open(['method'=>'post', 'action' => 'CartController@addToCartConfirmed']) !!}
-        <div class="col-sm-6 col-md-8">
-            <div class="row">
-                <p>{{ $product->description }}</p>
-                @include('purchase.partials.addToCart', ['product' => $product])
+    <div class="row">
+        <div class="col-md-2"></div>
+            <div class="col-sm-4">
+               <a href="/purchase/id-{{ $product->id }}"><img src="/images/brushpoint/purchase/{{ $product->sku }}-240.png" alt="{{ $product->name }}"></a>
             </div>
-          </div>
+            {!! Form::open(['method'=>'post', 'action' => 'CartController@addToCartConfirmed']) !!}
+            <div class="col-sm-4">
+                <div class="row">
+                    <p>{{ $product->description }}</p>
+                    @include('purchase.partials.addToCart', ['product' => $product])
+                </div>
+            </div>
+        <div class="col-md-2"></div>
         {!! Form::close() !!}
     </div>
 </div>
