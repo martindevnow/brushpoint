a2e7d793555aaa3595ebf59999642ac0ca23037e
Ben Martin (Cooler-PC) <martin.dev.now@gmail.com>
2015-04-28 18:21:27 -0400

    Cleaning up Confirm Add Page

--- resources/views/cart/confirmAdd.blade.php
+++ resources/views/cart/confirmAdd.blade.php
@@ -22,24 +22,28 @@
     <div class="row">
         <div class="col-md-2"></div>
         <div class="col-md-8">
-            <h2 class="left-text">{{ $product->name }}</h2>
-        </div>
-        <div class="col-md-2"></div>
-    </div>
-    <div class="row">
-        <div class="col-md-2"></div>
-            <div class="col-sm-4">
-               <a href="/purchase/id-{{ $product->id }}"><img src="/images/brushpoint/purchase/{{ $product->sku }}-240.png" alt="{{ $product->name }}"></a>
-            </div>
-            {!! Form::open(['method'=>'post', 'action' => 'CartController@addToCartConfirmed']) !!}
-            <div class="col-sm-4">
+            <div class="container thumbnail" style="width: 100%;">
                 <div class="row">
-                    <p>{{ $product->description }}</p>
-                    @include('purchase.partials.addToCart', ['product' => $product])
+                    <div class="col-md-8">
+                        <h2 class="left-text">{{ $product->name }}</h2>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-6">
+                       <a href="/purchase/id-{{ $product->id }}"><img src="/images/brushpoint/purchase/{{ $product->sku }}-240.png" alt="{{ $product->name }}"></a>
+                    </div>
+                    {!! Form::open(['method'=>'post', 'action' => 'CartController@addToCartConfirmed']) !!}
+                    <div class="col-sm-6">
+                        <div class="row">
+                            <p>{{ $product->description }}</p>
+                            @include('purchase.partials.addToCart', ['product' => $product])
+                        </div>
+                    </div>
+                    {!! Form::close() !!}
                 </div>
             </div>
+        </div>
         <div class="col-md-2"></div>
-        {!! Form::close() !!}
     </div>
 </div>
 @stop
