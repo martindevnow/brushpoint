dcd442d1cd8ee8c23114706479882f9f6be92da4
Ben Martin (Dell) <benjaminm@oriondas.com>
2015-05-28 16:08:03 -0400

    Fixed js slide images

--- resources/views/zeina/js/fadeslider.blade.php
+++ resources/views/zeina/js/fadeslider.blade.php
@@ -2,39 +2,39 @@
 <!-- optional touchswipe file to enable swipping to navigate slideshow -->
 <script type="text/javascript" src="/js/jquery.touchSwipe.min.js"></script>
 
-<style>
+    <style>
 
-#fadeshow4 .gallerylayer img{ /* make all images inside fadeshow4 scale to 100% of slideshow width */
-width: 100%;
-height: auto;
-}
+    #fadeshow4 .gallerylayer img{ /* make all images inside fadeshow4 scale to 100% of slideshow width */
+    width: 100%;
+    height: auto;
+    }
 
-</style>
+    </style>
 
-<script type="text/javascript" src="/js/fadeslideshow.js">
+    <script type="text/javascript" src="/js/fadeslideshow.js">
 
-/***********************************************
-* Ultimate Fade In Slideshow v2.0- (c) Dynamic Drive DHTML code library (www.dynamicdrive.com)
-* This notice MUST stay intact for legal use
-* Visit Dynamic Drive at http://www.dynamicdrive.com/ for this script and 100s more
-***********************************************/
+    /***********************************************
+    * Ultimate Fade In Slideshow v2.0- (c) Dynamic Drive DHTML code library (www.dynamicdrive.com)
+    * This notice MUST stay intact for legal use
+    * Visit Dynamic Drive at http://www.dynamicdrive.com/ for this script and 100s more
+    ***********************************************/
 
-</script>
+    </script>
 
-<script type="text/javascript">
+    <script type="text/javascript">
 
-    var mygallery = new fadeSlideShow({
-    wrapperid: "fadeshow1", //ID of blank DIV on page to house Slideshow
-            //dimensions: [600, 600], //width/height of gallery in pixels. Should reflect dimensions of largest image
-            dimensions: [525, 460], //width/height of gallery in pixels. Should reflect dimensions of largest image
-            imagearray: [
-                    ["http://brushpoint.dev/images/slideshow/about/dz.png"],
-                    ["http://brushpoint.dev/images/slideshow/about/sonic.png"] //<--no trailing comma after very last image element!
-            ],
-            displaymode: {type:'auto', pause:3500, cycles:0, wraparound:false},
-            persist: false, //remember last viewed slide and recall within same session?
-            fadeduration: 500, //transition duration (milliseconds)
-            descreveal: "always",
-            togglerid: ""
-            })
-</script>
+        var mygallery = new fadeSlideShow({
+        wrapperid: "fadeshow1", //ID of blank DIV on page to house Slideshow
+                //dimensions: [600, 600], //width/height of gallery in pixels. Should reflect dimensions of largest image
+                dimensions: [525, 460], //width/height of gallery in pixels. Should reflect dimensions of largest image
+                imagearray: [
+                        ["/images/slideshow/about/dz.png"],
+                        ["/images/slideshow/about/sonic.png"] //<--no trailing comma after very last image element!
+                ],
+                displaymode: {type:'auto', pause:3500, cycles:0, wraparound:false},
+                persist: false, //remember last viewed slide and recall within same session?
+                fadeduration: 500, //transition duration (milliseconds)
+                descreveal: "always",
+                togglerid: ""
+                })
+    </script>
