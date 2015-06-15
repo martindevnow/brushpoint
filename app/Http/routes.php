52dd031e945de7fa1b246b46f6e002c0da3bdf04
Ben Martin (Dell) <benjaminm@oriondas.com>
2015-06-15 12:54:51 -0400

    Added 301 redirect routes for the original brushpoint.com site

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -2,10 +2,83 @@
 
 Route::get('fileUplaod', function()
 {
-
     return view('testing.fileUpload');
-
 });
+
+
+/**
+ * Old Brushpoint Routes Redirect
+ */
+Route::get('capabilities.htm', function(){
+    return Redirect::to('/capabilities', 301);
+});
+Route::get('contact.htm', function(){
+    return Redirect::to('/feedback', 301);
+});
+Route::get('index.htm', function(){
+    return Redirect::to('/', 301);
+});
+Route::get('information.htm', function(){
+    return Redirect::to('/about', 301);
+});
+Route::get('products.htm', function(){
+    return Redirect::to('/products', 301);
+});
+Route::get('products_1.htm', function(){
+    return Redirect::to('/products/id-8', 301);
+});
+Route::get('products_10.htm', function(){
+    return Redirect::to('/products/id-7', 301);
+});
+Route::get('products_11.htm', function(){
+    return Redirect::to('/products/id-2', 301);
+});
+Route::get('products_12.htm', function(){
+    return Redirect::to('/products/id-3', 301);
+});
+Route::get('products_13.htm', function(){
+    return Redirect::to('/products/id-6', 301);
+});
+Route::get('products_14.htm', function(){
+    return Redirect::to('/products/id-14', 301);
+});
+Route::get('products_15.htm', function(){
+    return Redirect::to('/products/id-16', 301);
+});
+Route::get('products_16.htm', function(){
+    return Redirect::to('/products/id-1', 301);
+});
+Route::get('products_2.htm', function(){
+    return Redirect::to('/products/id-9', 301);
+});
+Route::get('products_3.htm', function(){
+    return Redirect::to('/products/id-10', 301);
+});
+Route::get('products_4.htm', function(){
+    return Redirect::to('/products/id-11', 301);
+});
+Route::get('products_5.htm', function(){
+    return Redirect::to('/products/id-12', 301);
+});
+Route::get('products_6.htm', function(){
+    return Redirect::to('/products/id-13', 301);
+});
+Route::get('products_7.htm', function(){
+    return Redirect::to('/products/id-1', 301);
+});
+Route::get('products_8.htm', function(){
+    return Redirect::to('/products/id-4', 301);
+});
+Route::get('products_9.htm', function(){
+    return Redirect::to('/products/id-5', 301);
+});
+Route::get('purchase.htm', function(){
+    return Redirect::to('/purchase', 301);
+});
+
+
+
+
 /**
  * June 11 2015
  * - TODO: When resizing the page, I changed at what size the large nav bar becomes the small one.
