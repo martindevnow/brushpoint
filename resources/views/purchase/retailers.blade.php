8415b1e9a6ef6ba80819c27da2cce292f0113cbb
BlueHost <benjaminm@oriondas.com>
2016-10-21 22:35:33 +0000

    Removed drugstore link as per Pauls request

--- resources/views/purchase/retailers.blade.php
+++ resources/views/purchase/retailers.blade.php
@@ -42,23 +42,23 @@ If you wish to purchase our toothbrushes, you can find links to them on the reta
         </div>
     </div>
     <div class="row">
-        <div class="col-md-3 col-sm-6">
+        <div class="col-md-4 col-sm-6">
             <a target="_blank" href="http://www.walmart.com/search/search-ng.do?query=BrushPoint&cat_id=976760&facet=retailer:Walmart.com">
                 <img class="img-responsive" src="/images/retailers/walmart.jpg" alt="Find BrushPoint products available at Wal-Mart.com"/>
             </a>
         </div>
-        <div class="col-md-3 col-sm-6">
+<!--        <div class="col-md-3 col-sm-6">
             <a target="_blank" href="http://www.drugstore.com/search/search_results.asp?N=0&Ntx=mode%2Bmatchallpartial&Ntk=All&srchtree=5&Ntt=BrushPoint&Go.x=15&Go.y=19">
                 <img class="img-responsive" src="/images/retailers/drugstore.jpeg" alt="BrushPoint products listed on Drugstore.com"/>
             </a>
         </div>
-        <div class="col-md-3 col-sm-6">
+-->        <div class="col-md-4 col-sm-6">
             <a target="_blank" href="http://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Dhpc&field-keywords=brushpoint">
             <!-- http://www.amazon.com/s?ie=UTF8&field-keywords=Brushpoint%20Innovations&index=blended&link_code=qs&sourceid=Mozilla-search&tag=mozilla-20 -->
                 <img class="img-responsive" src="/images/retailers/amazon.jpg" alt="BrushPoint products as found on Amazon.com"/>
             </a>
         </div>
-        <div class="col-md-3 col-sm-6">
+        <div class="col-md-4 col-sm-6">
             <a target="_blank" href="http://www.target.com/s?searchTerm=brushpoint&category=0%7CAll%7Cmatchallpartial%7Call+categories">
                 <img class="img-responsive" src="/images/retailers/target.gif" alt="BrushPoint products found on Target.com"/>
             </a>
